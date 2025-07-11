0c33f9c6a11f22bbb7288df0bd343efd7cbe96c5
Jen Alonso <jenalonso89@gmail.com>
2025-07-11 11:15:19 +0200

    vista publica

--- resources/views/components/layouts/app/header.blade.php
+++ resources/views/components/layouts/app/header.blade.php
@@ -1,3 +1,17 @@
+@php
+    $apartados = [
+        'Secciones'=>[
+            [
+                'name'=>'Home',
+                'icon'=>'home',
+                'url'=>route('home'),
+                'current'=>request()->routeIs('home')
+                
+            ]
+        ]
+    ]
+@endphp
+
 <!DOCTYPE html>
 <html lang="{{ str_replace('_', '-', app()->getLocale()) }}" class="dark">
     <head>
@@ -10,40 +24,19 @@
             <a href="{{ route('dashboard') }}" class="ms-2 me-5 flex items-center space-x-2 rtl:space-x-reverse lg:ms-0" wire:navigate>
                 <x-app-logo />
             </a>
-
+            @foreach ($apartados as $apartado => $secciones)
             <flux:navbar class="-mb-px max-lg:hidden">
-                <flux:navbar.item icon="layout-grid" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
-                    {{ __('Dashboard') }}
+                @foreach($secciones as $seccion)
+                <flux:navbar.item icon="layout-grid" :href="$seccion['url']" :current="$seccion['current']" wire:navigate>
+                    {{$seccion['name'] }}
                 </flux:navbar.item>
+                @endforeach
             </flux:navbar>
+            @endforeach
 
             <flux:spacer />
-
-            <flux:navbar class="me-1.5 space-x-0.5 rtl:space-x-reverse py-0!">
-                <flux:tooltip :content="__('Search')" position="bottom">
-                    <flux:navbar.item class="!h-10 [&>div>svg]:size-5" icon="magnifying-glass" href="#" :label="__('Search')" />
-                </flux:tooltip>
-                <flux:tooltip :content="__('Repository')" position="bottom">
-                    <flux:navbar.item
-                        class="h-10 max-lg:hidden [&>div>svg]:size-5"
-                        icon="folder-git-2"
-                        href="https://github.com/laravel/livewire-starter-kit"
-                        target="_blank"
-                        :label="__('Repository')"
-                    />
-                </flux:tooltip>
-                <flux:tooltip :content="__('Documentation')" position="bottom">
-                    <flux:navbar.item
-                        class="h-10 max-lg:hidden [&>div>svg]:size-5"
-                        icon="book-open-text"
-                        href="https://laravel.com/docs/starter-kits#livewire"
-                        target="_blank"
-                        label="Documentation"
-                    />
-                </flux:tooltip>
-            </flux:navbar>
-
             <!-- Desktop User Menu -->
+            @auth
             <flux:dropdown position="top" align="end">
                 <flux:profile
                     class="cursor-pointer"
@@ -86,35 +79,52 @@
                     </form>
                 </flux:menu>
             </flux:dropdown>
+           
+            @else
+            <flux:dropdown position="top" align="end">
+                <flux:button
+                    class="cursor-pointer"
+                    icon="user"
+                />
+
+                <flux:menu>
+
+                    <flux:menu.radio.group>
+                        <flux:menu.item :href="route('login')"  wire:navigate>{{ __('Log In') }}
+                        </flux:menu.item>
+                    </flux:menu.radio.group>
+                    <flux:menu.separator />
+                       <flux:menu.radio.group>
+                        <flux:menu.item :href="route('register')"  wire:navigate>{{ __('Register') }}
+                        </flux:menu.item>
+                    </flux:menu.radio.group>
+
+                </flux:menu>
+            </flux:dropdown>  
+            
+            @endauth
         </flux:header>
 
         <!-- Mobile Menu -->
         <flux:sidebar stashable sticky class="lg:hidden border-e border-zinc-200 bg-zinc-50 dark:border-zinc-700 dark:bg-zinc-900">
             <flux:sidebar.toggle class="lg:hidden" icon="x-mark" />
-
             <a href="{{ route('dashboard') }}" class="ms-1 flex items-center space-x-2 rtl:space-x-reverse" wire:navigate>
                 <x-app-logo />
             </a>
-
             <flux:navlist variant="outline">
+                @foreach ($apartados as $apartado=>$secciones)
                 <flux:navlist.group :heading="__('Platform')">
-                    <flux:navlist.item icon="layout-grid" :href="route('dashboard')" :current="request()->routeIs('dashboard')" wire:navigate>
-                    {{ __('Dashboard') }}
+                    @foreach($secciones as $seccion)
+                    <flux:navlist.item  :href="$seccion['url']" :current="$seccion['current']" wire:navigate>
+                    {{ $seccion['name'] }}
                     </flux:navlist.item>
+                    @endforeach
                 </flux:navlist.group>
+                @endforeach
             </flux:navlist>
 
             <flux:spacer />
 
-            <flux:navlist variant="outline">
-                <flux:navlist.item icon="folder-git-2" href="https://github.com/laravel/livewire-starter-kit" target="_blank">
-                {{ __('Repository') }}
-                </flux:navlist.item>
-
-                <flux:navlist.item icon="book-open-text" href="https://laravel.com/docs/starter-kits#livewire" target="_blank">
-                {{ __('Documentation') }}
-                </flux:navlist.item>
-            </flux:navlist>
         </flux:sidebar>
 
         {{ $slot }}
